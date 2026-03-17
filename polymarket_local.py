65f82ae005b32650515e3ec819a95cb4edd847c7
liuyebest <liuyebest@gmail.com>
2026-03-17 18:25:43 +0800

    Update polymarket_local.py

--- polymarket_local.py
+++ polymarket_local.py
@@ -6,8 +6,8 @@ import os
 import json
 
 # ============= 配置 =============
-TELEGRAM_TOKEN = "8409664041:AAEoTAL4QVmUb89mu24cGDhVEIl8q4UVPgM"
-TELEGRAM_CHAT_ID = "8590257714"
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 HISTORY_FILE = "market_history.json"
 
 EXCLUDED = ['sports','entertainment','pop-culture','esports','nba','nfl','nhl','mlb','ufc','soccer','football','tennis','golf','boxing','mma','movie','album','song','concert','festival','oscar','grammy','super bowl','lol','dota','csgo','valorant','gta','video game','gaming','playstation','xbox','nintendo']
